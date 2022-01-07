619b5ca26e10896172f02003f7682b9b0e109e10
samuelmyoun <samuelmyoun@gmail.com>
2022-01-07 18:02:49 -0500

    Added menu to top of home page

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './Website.css';
 
 //These statements import all of the react components
 import Menu from './HomeComponents/Menu.js';
@@ -9,11 +8,9 @@ class Home extends Component {
 
 render(){
   return(
-    <body>
-      <div>
-        Hello
-      </div>
-    </body>
+    <div>
+      <Menu />
+    </div>
     );
   }
 }
--- src/HomeComponents/Menu.js
+++ src/HomeComponents/Menu.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import './components.css';
 
 
 class Menu extends Component {
 
-  constructor(){
+  constructor(props){
     super(props);
+    this.redirHome = this.redirHome.bind(this);
     this.redirAboutMe = this.redirAboutMe.bind(this);
+    this.redirContact = this.redirContact.bind(this);
+    this.redirResume = this.redirResume.bind(this);
     this.state = {};
   }
 
+  redirHome() {
+
+  }
+
   redirAboutMe() {
+
+  }
+
+  redirContact() {
+
+  }
+
+  redirResume() {
 
   }
 
@@ -18,7 +34,10 @@ class Menu extends Component {
   render(){
     return(
         <div>
-          <button onClick={this.redirAboutMe}>About Me</button>
+          <button class= "button" onClick={this.redirHome}>Home</button>
+          <button class= "button" onClick={this.redirAboutMe}>About Me</button>
+          <button class= "button" onClick={this.redirContact}>Contact</button>
+          <button class= "button" onClick={this.redirResume}>Resume</button>
         </div>
       );
     }
